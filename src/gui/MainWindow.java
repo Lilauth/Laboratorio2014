532caa64cd2c07f2fff4f9fb52bfedbfa120d0e7
Laura Lus <mlaura.lus@gmail.com>
2014-10-28 20:08:03 -0300

    Más fixes

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -1,14 +1,19 @@
 package gui;
 
+import gui.TableDemo.MyTableModel;
+
+import java.awt.Dimension;
 import java.awt.EventQueue;
+
 import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JButton;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.table.AbstractTableModel;
 
 import model.Categoria;
 import model.Categorias;
@@ -17,9 +22,103 @@ import model.Etiquetas;
 import model.Modelo;
 
 public class MainWindow {
-
 	private JFrame frame;
 	private JTable table;
+	private boolean DEBUG = false;
+	class MyTableModel extends AbstractTableModel {
+        private String[] columnNames = {"First Name",
+                                        "Last Name",
+                                        "Sport",
+                                        "# of Years",
+                                        "Vegetarian"};
+        private Object[][] data = {
+	    {"Kathy", "Smith",
+	     "Snowboarding", new Integer(5), new Boolean(false)},
+	    {"John", "Doe",
+	     "Rowing", new Integer(3), new Boolean(true)},
+	    {"Sue", "Black",
+	     "Knitting", new Integer(2), new Boolean(false)},
+	    {"Jane", "White",
+	     "Speed reading", new Integer(20), new Boolean(true)},
+	    {"Joe", "Brown",
+	     "Pool", new Integer(10), new Boolean(false)}
+        };
+
+        public int getColumnCount() {
+            return columnNames.length;
+        }
+
+        public int getRowCount() {
+            return data.length;
+        }
+
+        public String getColumnName(int col) {
+            return columnNames[col];
+        }
+
+        public Object getValueAt(int row, int col) {
+            return data[row][col];
+        }
+
+        /*
+         * JTable uses this method to determine the default renderer/
+         * editor for each cell.  If we didn't implement this method,
+         * then the last column would contain text ("true"/"false"),
+         * rather than a check box.
+         */
+        public Class getColumnClass(int c) {
+            return getValueAt(0, c).getClass();
+        }
+
+        /*
+         * Don't need to implement this method unless your table's
+         * editable.
+         */
+        public boolean isCellEditable(int row, int col) {
+            //Note that the data/cell address is constant,
+            //no matter where the cell appears onscreen.
+            if (col < 2) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        /*
+         * Don't need to implement this method unless your table's
+         * data can change.
+         */
+        public void setValueAt(Object value, int row, int col) {
+            if (DEBUG) {
+                System.out.println("Setting value at " + row + "," + col
+                                   + " to " + value
+                                   + " (an instance of "
+                                   + value.getClass() + ")");
+            }
+
+            data[row][col] = value;
+            fireTableCellUpdated(row, col);
+
+            if (DEBUG) {
+                System.out.println("New value of data:");
+                printDebugData();
+            }
+        }
+
+        private void printDebugData() {
+            int numRows = getRowCount();
+            int numCols = getColumnCount();
+
+            for (int i=0; i < numRows; i++) {
+                System.out.print("    row " + i + ":");
+                for (int j=0; j < numCols; j++) {
+                    System.out.print("  " + data[i][j]);
+                }
+                System.out.println();
+            }
+            System.out.println("--------------------------");
+        }
+    }
 
 	/**
 	 * Launch the application.
@@ -87,18 +186,11 @@ public class MainWindow {
 		panel_1.setBounds(0, 43, 537, 251);
 		frame.getContentPane().add(panel_1);
 		panel_1.setLayout(null);
-		
-		table = new JTable();
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null, null},
-				{null, null, null},
-				{null, null, null},
-			},
-			new String[] {
-				"Emisor", "Detalle", "Etiqueta"
-			}
-		));
+		/*JTable table = new JTable(new MyTableModel());
+	    table.setPreferredScrollableViewportSize(new Dimension(500, 70));
+	    table.setFillsViewportHeight(true);*/
+	    
+		JTable table = new JTable(new MyTableModel());
 		table.setBounds(133, 0, 404, 251);
 		panel_1.add(table);
 		//lista de etiquetas+		
@@ -106,7 +198,7 @@ public class MainWindow {
 		for (Etiqueta et: ets.getEtiquetas()){
 			listModel.addElement(et);
 		}
-				
+        
 		JList list = new JList(listModel);
 		//JList list = new JList();		
 		list.setBounds(0, -1, 129, 84);
