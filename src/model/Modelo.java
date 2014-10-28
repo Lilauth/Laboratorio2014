a2da1ab5c49e0ba9b3e10ff462ea5eb1d06d1c28
Laura Lus <mlaura.lus@gmail.com>
2014-10-28 20:13:21 -0300

    a

--- src/model/Modelo.java
+++ src/model/Modelo.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 //import java.util.Date;
 //import java.text.DateFormat;
 //import java.text.ParseException;
+
 //import java.text.SimpleDateFormat;
 
 public class Modelo {
